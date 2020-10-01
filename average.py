8a906cae7bc17516a675d7bbe1ecde130053ed8a
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-10-01 19:30:15 +0000

    Update average.py

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by Marcus A. Mosley
-# Created on September 2020
+# Created on October 2020
 # This program finds the average of three integers,
 #     only if they are between 0-100 inclusive
 
